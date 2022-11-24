244cea1519441f67b251369581ce516401d74a32
nelchacin <nelchacindev@gmail.com>
2022-11-24 01:18:07 +0100

    4 commit js

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 const selectorButtons = document.querySelectorAll('.btn');
 const ShoppingCardItemsContainer = document.querySelector('.shoppingCard')
 
+
+
 selectorButtons.forEach((addToCardButton) => {
     addToCardButton.addEventListener('click', addToCardClicked)
 });
 
 function addToCardClicked(event) {
+    event.preventDefault()
     const button = event.target;
     const item = button.closest('.card-body');
 
@@ -13,24 +16,45 @@ function addToCardClicked(event) {
     const priceProduct = item.querySelector('.price').textContent * 1;
     const imgProduct = item.querySelector('.imgSec4').src;
 
-    addItemtoShoppingCard(tittleProduct, priceProduct, imgProduct)
+    addItemtoShoppingCard(tittleProduct, priceProduct.toFixed(2), imgProduct)
 }
 
 function addItemtoShoppingCard(tittleProduct, priceProduct, imgProduct) {
-    const shoppingCardRow = document.createElement('div');
-    const shoppingCardContent = `
-    <div class='productShopping'>
-            <img src="${imgProduct}" alt="">
-            <p>"${tittleProduct}"</p>
-            <p>"${priceProduct}"</p>
-            <p>"Cantidad"</p>
-            <button>x</button>
-            <p>"Total"</p>
-    </div>
-  `;
-    shoppingCardRow.innerHTML = shoppingCardContent
-    ShoppingCardItemsContainer.append(shoppingCardRow)
+    const shoppingCardRow = document.createElement('div')
+    shoppingCardRow.classList.add("itemInTheCard")
 
+    const shoppingCardContent = `
+    <div class="row shoppingCartItem">
+    <div class="col-6">
+        <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+            <img src=${imgProduct} class="shopping-cart-image">
+            <h6 class="shopping-cart-item-title shoppingCartItemTitle text-truncate ml-3 mb-0">${tittleProduct}</h6>
+        </div>
+    </div>
+    <div class="col-2">
+        <div class="shopping-cart-price d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+            <p class="item-price mb-0 shoppingCartItemPrice">${priceProduct}€</p>
+        </div>
+    </div>
+    <div class="col-4">
+        <div
+            class="shopping-cart-quantity d-flex justify-content-between align-items-center h-100 border-bottom pb-2 pt-3">
+            <input class="shopping-cart-quantity-input shoppingCartItemQuantity" type="number"
+                value="1">
+            <button class="btn btn-danger buttonDelete" type="button">X</button>
+        </div>
+    </div>
+</div>`;
+    ;
+    shoppingCardRow.innerHTML = shoppingCardContent
+    ShoppingCardItemsContainer.appendChild(shoppingCardRow)
+    console.log(ShoppingCardItemsContainer);
+    updateShoppingCardTotal()
+
+}
+
+function updateShoppingCardTotal() {
+    let total = 0
 }
 
 
